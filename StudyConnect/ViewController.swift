0541a4f5e6b50f89bd7d6ab111238a16e95218c3
sarahrittgers26 <s.r.rittgers@gmail.com>
2018-11-12 17:40:37 -0500

    init

---
+++ StudyConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyConnect
+//
+//  Created by Sarah Rittgers on 11/12/18.
+//  Copyright © 2018 Sarah Rittgers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
